a22c9a2149503670d061c8dd35bedc785defc9c8
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-04-15 11:23:31 +0800

    added domain in output

--- Get-ActiveDirectoryUsers.ps1
+++ Get-ActiveDirectoryUsers.ps1
@@ -1,7 +1,12 @@
 $properties = 'SamAccountName', 'EmailAddress', 'EmployeeID'
 $adUsers = Get-ADUser -Filter * -Properties $properties | Where-Object EmailAddress -ne $null
+$domain = (Get-CimInstance Win32_ComputerSystem).Domain
+
 $adUsers | % {
     $user = $_
+    
+    Write-Host "$domain," -NoNewline
+
     $properties | % {
         $property = $_
         Write-Host "$($user.$property)," -NoNewline
