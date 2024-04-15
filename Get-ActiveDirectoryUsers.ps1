13f7be71f5c57ddc28357ed0184df8f4eecf98f7
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-04-15 11:27:21 +0800

    removed null email address filter

--- Get-ActiveDirectoryUsers.ps1
+++ Get-ActiveDirectoryUsers.ps1
@@ -1,5 +1,5 @@
 $properties = 'SamAccountName', 'EmailAddress', 'EmployeeID'
-$adUsers = Get-ADUser -Filter * -Properties $properties | Where-Object EmailAddress -ne $null
+$adUsers = Get-ADUser -Filter * -Properties $properties
 $domain = (Get-CimInstance Win32_ComputerSystem).Domain
 
 $adUsers | % {
