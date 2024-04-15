b17dd5b16deff3dded0b229643cb7e87301444bf
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-04-15 10:03:16 +0800

    added download from s3

--- get_aws_ad_users.py
+++ get_aws_ad_users.py
@@ -1,7 +1,7 @@
 import boto3
 import time
 import sys
-import csv
+#import csv
 from datetime import datetime
 
 
@@ -32,12 +32,14 @@ session = boto3.Session(region_name=region)
 
 # Use the SSM (Simple Systems Manager) client
 ssm = session.client('ssm')
+s3 = session.client('s3')
 
 # Define the PowerShell script you want to run
 # Load the PowerShell script from a file
 with open('Get-ActiveDirectoryUsers.ps1', 'r') as file:
     powershell_script = file.read()
-    
+
+# need to add code to verify s3 bucket
 target_bucket = f'infrasre-adreport-raw-{domain.lower()}'
 
 # Send the command
@@ -56,7 +58,6 @@ command_id = response['Command']['CommandId']
 invocation_response = wait_for_command_to_complete(instance_id, command_id)
 ad_users_str = invocation_response['StandardOutputContent'].splitlines()
 
-
 # Get the current date
 current_date = datetime.now()
 
@@ -70,44 +71,61 @@ domain = domain.replace(" ", "")
 csv_file_name = f"AD{domain}_{formatted_date}.csv"
 
 # Define the header names based on the data we are collecting
-headers = ['Domain', 'SamAccountName', 'EmailAddress', 'EmployeeID']
+headers = 'Domain,SamAccountName,EmailAddress,EmployeeID'
+
+#get output from s3
+# s3_client = boto3.client('s3')
+s3_download_path = f'{command_id}/{instance_id}/awsrunPowerShellScript/0.awsrunPowerShellScript/
+stdout'
+s3.download_file(target_bucket, s3_download_path, csv_file_name)
+
+# Step 1: Read the existing content
+with open(csv_file_name, 'r') as file:
+    original_content = file.readlines()
+
+# Step 2: Add the new lines at the beginning
+new_content = headers + original_content
+
+# Step 3: Write the updated content back to the file
+with open(csv_file_name, 'w') as file:
+    file.writelines(new_content)
 
 # Open a new CSV file
-with open(csv_file_name, mode='w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=headers)
+#with open(csv_file_name, mode='w', newline='') as file:
+    # writer = csv.DictWriter(file, fieldnames=headers)
     
-    # Write the header
-    writer.writeheader()
+    # # Write the header
+    # writer.writeheader()
 
-    for ad_user in ad_users_str:
-        ad_user_properties = ad_user.split(',')
+    # for ad_user in ad_users_str:
+    #     ad_user_properties = ad_user.split(',')
         
-        # Initialize variables with None or default values
-        sam_account_name = None
-        # display_name = None
-        email = None
-        employee_id = None
+    #     # Initialize variables with None or default values
+    #     sam_account_name = None
+    #     # display_name = None
+    #     email = None
+    #     employee_id = None
 
-        # Conditional assignments
-        if ad_user_properties[0].strip():
-            sam_account_name = ad_user_properties[0].strip()
-        if len(ad_user_properties) > 1 and ad_user_properties[1].strip():
-            email = ad_user_properties[1].strip()
-        if len(ad_user_properties) > 2 and ad_user_properties[2].strip():
-            employee_id = ad_user_properties[2].strip()
-        # if len(ad_user_properties) > 3 and ad_user_properties[3].strip():
-        #     account_expiration_date = ad_user_properties[3].strip()
+    #     # Conditional assignments
+    #     if ad_user_properties[0].strip():
+    #         sam_account_name = ad_user_properties[0].strip()
+    #     if len(ad_user_properties) > 1 and ad_user_properties[1].strip():
+    #         email = ad_user_properties[1].strip()
+    #     if len(ad_user_properties) > 2 and ad_user_properties[2].strip():
+    #         employee_id = ad_user_properties[2].strip()
+    #     # if len(ad_user_properties) > 3 and ad_user_properties[3].strip():
+    #     #     account_expiration_date = ad_user_properties[3].strip()
         
-        # Write the user's details to the CSV
-        writer.writerow({
-            'Domain': domain,
-            'SamAccountName': sam_account_name,
-            'EmailAddress': email,
-            'EmployeeID': employee_id
-        })
+    #     # Write the user's details to the CSV
+    #     writer.writerow({
+    #         'Domain': domain,
+    #         'SamAccountName': sam_account_name,
+    #         'EmailAddress': email,
+    #         'EmployeeID': employee_id
+    #     })
         
-        # print(f"{domain},{sam_account_name},{display_name},{email},{account_expiration_date}")
-        print(f"{domain},{sam_account_name},{email},{employee_id}")
+    #     # print(f"{domain},{sam_account_name},{display_name},{email},{account_expiration_date}")
+    #     print(f"{domain},{sam_account_name},{email},{employee_id}")
 
     
 
