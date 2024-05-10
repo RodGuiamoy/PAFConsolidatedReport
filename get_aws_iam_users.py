f98033d7e775f6c73f9d6fd163729bf722bc93c5
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-05-10 10:36:51 -0400

    used paginator for aws iam

--- get_aws_iam_users.py
+++ get_aws_iam_users.py
@@ -14,7 +14,7 @@ def get_user_tags(username):
     return employee_id, email
 
 def main(aws_environment):
-    users = iam.list_users()['Users']
+    # users = iam.list_users()['Users']
     account_id = boto3.client('sts').get_caller_identity().get('Account')
     
     # Get the current date
@@ -39,22 +39,26 @@ def main(aws_environment):
         # Write the header
         writer.writeheader()
         
-        # Iterate over each user and write their information as a row in the CSV
-        for user in users:
-            username = user['UserName']
-            employee_id, email = get_user_tags(username)
-            created_date = user['CreateDate'].strftime('%Y-%m-%d %H:%M:%S')
-            
-            # Write the user's details to the CSV
-            writer.writerow({
-                'UserName': username,
-                'EmployeeID': employee_id,
-                'Email': email,
-                'CreateDate': created_date,
-                'AccountID': account_id
-            })
-            
-            print (f"{username},{employee_id},{email},{created_date},{account_id}")
+        # Assuming iam is a configured AWS IAM client
+        paginator = iam.get_paginator('list_users')
+        for response in paginator.paginate():
+            for user in response['Users']:
+                # # Iterate over each user and write their information as a row in the CSV
+                # for user in users:
+                username = user['UserName']
+                employee_id, email = get_user_tags(username)
+                created_date = user['CreateDate'].strftime('%Y-%m-%d %H:%M:%S')
+                
+                # Write the user's details to the CSV
+                writer.writerow({
+                    'UserName': username,
+                    'EmployeeID': employee_id,
+                    'Email': email,
+                    'CreateDate': created_date,
+                    'AccountID': account_id
+                })
+                
+                print (f"{username},{employee_id},{email},{created_date},{account_id}")
 
 if __name__ == "__main__":
     aws_environment = sys.argv[1]
