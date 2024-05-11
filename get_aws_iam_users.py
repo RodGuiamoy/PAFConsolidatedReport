80ea08c109469009e681818e33bbeaea89af44d8
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-05-11 05:09:18 -0400

    added groups and policies

--- get_aws_iam_users.py
+++ get_aws_iam_users.py
@@ -13,6 +13,12 @@ def get_user_tags(username):
     email = next((tag['Value'] for tag in tags if tag['Key'] == 'email'), None)
     return employee_id, email
 
+def get_group_attached_policies(group_name):
+    """Retrieve policies attached to a group."""
+    attached_policies_response = iam.list_attached_group_policies(GroupName=group_name)
+    attached_policies = [policy['PolicyName'] for policy in attached_policies_response['AttachedPolicies']]
+    return attached_policies
+
 def main(aws_environment):
     # users = iam.list_users()['Users']
     account_id = boto3.client('sts').get_caller_identity().get('Account')
@@ -30,7 +36,7 @@ def main(aws_environment):
     csv_file_name = f"AWS{aws_environment}_{formatted_date}.csv"
     
     # Define the header names based on the data we are collecting
-    headers = ['UserName', 'EmployeeID', 'Email', 'CreateDate', 'AccountID']
+    headers = ['UserName', 'EmployeeID', 'Email', 'CreateDate', 'Groups', 'GroupAttachedPolicies', 'AttachedPolicies', 'InlinePolicies', 'AccountID']
     
     # Open a new CSV file
     with open(csv_file_name, mode='w', newline='') as file:
@@ -49,18 +55,38 @@ def main(aws_environment):
                 employee_id, email = get_user_tags(username)
                 created_date = user['CreateDate'].strftime('%Y-%m-%d %H:%M:%S')
                 
+                # Get groups for the user
+                groups_response = iam.list_groups_for_user(UserName=username)
+                groups = [group['GroupName'] for group in groups_response['Groups']]
+                
+                # Get attached policies for the user
+                attached_policies_response = iam.list_attached_user_policies(UserName=username)
+                attached_policies = [policy['PolicyName'] for policy in attached_policies_response['AttachedPolicies']]
+                
+                 # Get inline policies for the user
+                inline_policies_response = iam.list_user_policies(UserName=username)
+                inline_policies = [policy_name for policy_name in inline_policies_response['PolicyNames']]
+                
+                # Get policies attached via groups for the user
+                group_attached_policies = []
+                for group_name in groups:
+                    group_attached_policies.extend(get_group_attached_policies(group_name))
+                
                 # Write the user's details to the CSV
                 writer.writerow({
                     'UserName': username,
                     'EmployeeID': employee_id,
                     'Email': email,
                     'CreateDate': created_date,
+                    'Groups': ','.join(groups),
+                    'GroupAttachedPolicies': ','.join(group_attached_policies),
+                    'AttachedPolicies': ','.join(attached_policies),
+                    'InlinePolicies': ','.join(inline_policies),
                     'AccountID': account_id
                 })
                 
-                print (f"{username},{employee_id},{email},{created_date},{account_id}")
+                print (f"{username},{employee_id},{email},{created_date},{','.join(groups)},{','.join(attached_policies)},{','.join(inline_policies)},{','.join(group_attached_policies)},{account_id}")
 
 if __name__ == "__main__":
     aws_environment = sys.argv[1]
     main(aws_environment)
-    
