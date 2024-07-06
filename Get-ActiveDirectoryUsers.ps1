b74d549da2d55dcb57750069da43914f5b48b5f8
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-06 09:23:34 -0400

    trying to shorten the runtime of aws ad

--- Get-ActiveDirectoryUsers.ps1
+++ Get-ActiveDirectoryUsers.ps1
@@ -1,34 +1,36 @@
-$properties = 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate'
-$headers = ($properties -join ",") + ',Domain,OU,GroupMemberships'
-
+#Measure-Command {
+$properties = 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', 'MemberOf'
+$headers = 'Domain,' + ($properties -join ",") + ',OU'
+    
 $adUsers = Get-ADUser -Filter * -Properties $properties
 $domain = (Get-CimInstance Win32_ComputerSystem).Domain
-
+    
 $userList = ""
-
+    
 $adUsers | % {
-
+    
     $user = $_
-
+    
     $propertyValues = @()
-
-    foreach ($prop in $properties) {
-        $propertyValues += "$($user.$prop)"
+    
+    $properties | Where-Object { $_ -ne 'MemberOf' } | % {
+        
+        $propertyValues += $user.$_
+    
     }
-
+    
     # Join all property value strings with a comma
     $outputString = $propertyValues -join ","
-
+    
     # Get the OU from the DistinguishedName
     $ou = $user.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)'
-    # Get the group memberships
-    $groupNames = (Get-ADPrincipalGroupMembership $user | Select-Object Name).Name -join ","
-        
-    # Write-Host $outputString + ",$domain,`"$ou`",`"$groupNames`""
-    $userList = ($userList + $outputString + ",$domain,`"$ou`",`"$groupNames`"`n")
-
-    # Read-Host
+    
+    $groupMemberships = $user.MemberOf -join "; "
+            
+    $userList = ($userList + "$domain," + $outputString + ",`"$groupMemberships`"" + ",`"$ou`"`n")
+    
 }
-
+    
 $headers
 $userList
+    
