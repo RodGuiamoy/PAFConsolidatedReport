03616b929a87e4f96d640fba7519236d2f9d4f15
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-11 05:57:26 -0400

    new solution for aws ad

--- Get-AWSActiveDirectoryUsers.ps1
+++ Get-AWSActiveDirectoryUsers.ps1
@@ -10,7 +10,7 @@ Function Invoke-Main {
 
     $adUsers | `
         Select-Object @{Name = 'Domain'; Expression = { $domain } }, 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', @{Name='GroupMemberships'; Expression = { $_.MemberOf -join "; "}}, @{Name = 'OU'; Expression = { $_.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)' } } | `
-        Export-CSV "$domain.csv"
+        Export-CSV "$domain.csv" -NoTypeInformation -Encoding UTF8
 
     $cmd = 'curl -X PUT -T "{0}" --ssl-no-revoke "{1}"' -f "$domain.csv", $S3PresignedUploadUrl
     cmd /c $cmd
