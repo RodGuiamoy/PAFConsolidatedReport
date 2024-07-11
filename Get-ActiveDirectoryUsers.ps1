3e2faddb2a120360e576982eb475844abfd06301
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-11 02:16:46 -0400

    putting back old fields for AD

--- Get-ActiveDirectoryUsers.ps1
+++ Get-ActiveDirectoryUsers.ps1
@@ -1,5 +1,5 @@
-$properties = 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', 'MemberOf'
-$headers = 'Domain,' + ($properties -join ",") + ',OU,GroupMemberships'
+$properties = 'SamAccountName', 'EmailAddress', 'EmployeeID'#, 'LastLogonDate', 'MemberOf'
+$headers = 'Domain,' + ($properties -join ",") #+ ',OU,GroupMemberships'
 
 $adUsers = Get-ADUser -Filter * -Properties $properties
 $domain = (Get-CimInstance Win32_ComputerSystem).Domain
@@ -11,12 +11,13 @@ $adUsers | % {
     $user = $_
 
 
-    # Get the OU from the DistinguishedName
-    $ou = $user.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)'
+    # # Get the OU from the DistinguishedName
+    # $ou = $user.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)'
 
-    $groupMemberships = $user.MemberOf -join ";"
+    # $groupMemberships = $user.MemberOf -join ";"
         
-    $userList = ($userList + "$domain," + "$($user.SamAccountName),$($user.EmailAddress),$($user.EmployeeID),$($user.LastLogonDate)," + "`"$ou`",`"$groupMemberships`"`n")
+    #$userList = ($userList + "$domain," + "$($user.SamAccountName),$($user.EmailAddress),$($user.EmployeeID),$($user.LastLogonDate)," + "`"$ou`",`"$groupMemberships`"`n")
+    $userList = ($userList + "$domain," + "$($user.SamAccountName),$($user.EmailAddress),$($user.EmployeeID)" + "`n") #,$($user.LastLogonDate)," + "`"$ou`",`"$groupMemberships`"`n")
 
 }
 
