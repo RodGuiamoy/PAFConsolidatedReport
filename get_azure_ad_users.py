f4d06754ab34662a08802edfcc0cd6f8e1d0f9ab
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-13 06:34:11 -0400

    ?

--- get_azure_ad_users.py
+++ get_azure_ad_users.py
@@ -38,17 +38,9 @@ base_url = "https://management.azure.com"
 
 new_token = generate_token(azure_client_id, azure_client_secret)
 
-# base param set
-# sub_id = "ca451cef-a9cc-4e8e-b9eb-f35f39fdac77"
-# target_vm = "ADDC2"
 api_version = "2023-09-01"
-# resource_groups = ["productionprimary", "ProductionSecondary"]
 
 headers = {"Authorization": f"Bearer {new_token}", "Content-Type": "application/json"}
-
-# script is defined here
-
-# post_data = {"commandId":"RunPowerShellScript","script":["# Get all users from Active Directory\r\n$allUsers = Get-ADUser -Filter * -Properties SamAccountName, DisplayName, EmailAddress, AccountExpirationDate, Title\r\n \r\n# required variables\r\n$emailInput = \""+target_email+"\"\r\n$newExpiryDate = \""+cutoffdate+"\"\r\n$newTitle = \"AutoExpireEnabled\"\r\n \r\n# Filter users based on the input email address\r\n$matchingUsers = $allUsers | Where-Object { $_.EmailAddress -eq $emailInput }\r\n \r\n# Check if any users were found\r\nif ($matchingusers -ne $null) {\r\n    # Display header before update\r\n    Write-Host \"Before Update - SamAccountName`tDisplayName`tEmailAddress`tAccountExpiry`tTitle\"\r\n \r\n    # Display user details before update\r\n    foreach ($user in $matchingUsers) {\r\n        $samAccountName = $user.SamAccountName\r\n        $displayName = $user.DisplayName\r\n        $emailAddress = $user.EmailAddress\r\n        $accountExpiry = $user.AccountExpirationDate\r\n        $title = $user.Title\r\n \r\n        Write-Host \"$samAccountName`t$displayName`t$emailAddress`t$accountExpiry`t$title\"\r\n    }\r\n \r\n    # Separate message\r\n    Write-Host \"---------------------\"\r\n    Write-Host \"Updating user attributes...\"\r\n \r\n \r\n    # Update account attributes\r\n    foreach ($user in $matchingUsers) {\r\n        $samAccountName = $user.SamAccountName\r\n \r\n        # Update account expiration date and title\r\n        Set-ADUser -Identity $samAccountName -AccountExpirationDate $newExpiryDate -Title $newTitle\r\n    }\r\n \r\n    Start-Sleep -Seconds 10\r\n \r\n    # Display header after update\r\n    Write-Host \"After Update - SamAccountName`tDisplayName`tEmailAddress`tAccountExpiry`tTitle\"\r\n \r\n    # Display user details after update\r\n    foreach ($user in $matchingUsers){\r\n    # Fetch the latest user details after the update\r\n    $updatedUser = Get-ADUser -Identity $user.SamAccountName -Properties SamAccountName, DisplayName, EmailAddress, AccountExpirationDate, Title\r\n     \r\n        $samAccountName = $updatedUser.SamAccountName\r\n        $displayName = $updatedUser.DisplayName\r\n        $emailAddress = $updatedUser.EmailAddress\r\n        $accountExpiry = $updatedUser.AccountExpirationDate\r\n        $title = $updatedUser.Title\r\n \r\n        Write-Host \"$samAccountName`t$displayName`t$emailAddress`t$accountExpiry`t$title\"\r\n    }\r\n} else {\r\n    Write-Host \"No users found with the provided email address.\"\r\n}"]}
 
 # Define the PowerShell script you want to run
 # Load the PowerShell script from a file
@@ -81,9 +73,18 @@ if location:
 
     run_result_response = requests.get(location, headers=headers).json()["value"]
 
-    ad_users = run_result_response[0]["message"]
+    # ad_users = run_result_response[0]["message"]
     
-    print(run_result_response)
+    # print(run_result_response)
+    ad_users = ""
+    if run_result_response:
+    
+        for result in run_result_response:
+            ad_users += result.get("message", "")
+            
+    print("Result from PowerShell script:")
+    print(ad_users)
+
     
 else:
     print("\n=======================================================")
