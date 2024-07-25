9f1028c70fb1322a90c269b93d35a6ae2d0ddea5
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-07-25 10:27:55 -0400

    removed pingdom

--- get_external_tool_users.py
+++ get_external_tool_users.py
@@ -7,45 +7,45 @@ from sendgrid import SendGridAPIClient
 from datetime import datetime
 import duo_client
 
-def get_pingdom_users(api_id, api_key):
-    base_url = "https://api.pingdom.com/api/3.1/alerting/contacts"
-    headers = {"Authorization":f"Bearer {api_key}"}
+# def get_pingdom_users(api_id, api_key):
+#     base_url = "https://api.pingdom.com/api/3.1/alerting/contacts"
+#     headers = {"Authorization":f"Bearer {api_key}"}
 
-    user_list = requests.get(base_url, headers=headers).json()
-    # print(user_list)
+#     user_list = requests.get(base_url, headers=headers).json()
+#     # print(user_list)
     
-    # Get the current date
-    current_date = datetime.now()
+#     # Get the current date
+#     current_date = datetime.now()
 
-    # Format the date to MMddyyyy
-    formatted_date = current_date.strftime('%m%d%Y')
+#     # Format the date to MMddyyyy
+#     formatted_date = current_date.strftime('%m%d%Y')
     
-    # Define the CSV file name
-    csv_file_name = f"Pingdom_{formatted_date}.csv"
+#     # Define the CSV file name
+#     csv_file_name = f"Pingdom_{formatted_date}.csv"
 
-    # Define the header names based on the data we are collecting
-    headers = ['id', 'name', 'email']
-    # Open a new CSV file
-    with open(csv_file_name, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=headers)
+#     # Define the header names based on the data we are collecting
+#     headers = ['id', 'name', 'email']
+#     # Open a new CSV file
+#     with open(csv_file_name, mode='w', newline='') as file:
+#         writer = csv.DictWriter(file, fieldnames=headers)
         
-        # Write the header
-        writer.writeheader()
+#         # Write the header
+#         writer.writeheader()
         
-        # Iterate over each user and write their information as a row in the CSV
-        for user in user_list['contacts']:
-            id = user['id']
-            name = user['name']
-            email = user['notification_targets']['email'][0]['address']
+#         # Iterate over each user and write their information as a row in the CSV
+#         for user in user_list['contacts']:
+#             id = user['id']
+#             name = user['name']
+#             email = user['notification_targets']['email'][0]['address']
             
-            # Write the user's details to the CSV
-            writer.writerow({
-                'id': id,
-                'name': name,
-                'email': email
-            })
+#             # Write the user's details to the CSV
+#             writer.writerow({
+#                 'id': id,
+#                 'name': name,
+#                 'email': email
+#             })
             
-            print(f"{id}, {name}, {email}")
+#             print(f"{id}, {name}, {email}")
 
 def get_pagerduty_users(api_id, api_key):
    
@@ -253,7 +253,7 @@ api_key = api_key.replace("\n","").strip()
 
 # Define the switch-case dictionary
 external_tool_functions = {
-    "Pingdom": get_pingdom_users,
+    # "Pingdom": get_pingdom_users,
     "PagerDuty": get_pagerduty_users,
     "SendGrid": get_sendgrid_users,
     "Site24x7": get_site24x7_users,
