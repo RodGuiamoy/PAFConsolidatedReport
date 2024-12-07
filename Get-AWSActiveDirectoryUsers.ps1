fa392dc1810c19e7459398debcfc309f04be6ac5
RodGuiamoy <janrudolfguiamoy@gmail.com>
2024-12-07 14:41:19 -0500

    additional properties to ad scripts

--- Get-AWSActiveDirectoryUsers.ps1
+++ Get-AWSActiveDirectoryUsers.ps1
@@ -4,7 +4,7 @@ Function Invoke-Main {
         $CSVFileName
     )
 
-    $properties = 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', 'MemberOf', 'DistinguishedName'
+    $properties = 'Name', 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', 'MemberOf', 'DistinguishedName', 'whenCreated', 'whenChanged'
 
     $adUsers = Get-ADUser -Filter * -Properties $properties
     $domain = (Get-CimInstance Win32_ComputerSystem).Domain
@@ -12,7 +12,13 @@ Function Invoke-Main {
     # Remove-Item "$domain.csv" -Force
 
     $adUsers | `
-        Select-Object @{Name = 'Domain'; Expression = { $domain } }, 'SamAccountName', 'EmailAddress', 'EmployeeID', 'LastLogonDate', @{Name = 'GroupMemberships'; Expression = { $_.MemberOf -join "; " } }, @{Name = 'OU'; Expression = { $_.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)' } }, @{Name = 'AccountStatus'; Expression = { if ($_.Enabled -eq $True) { 'Enabled' } else { 'Disabled' } } } | `
+        Select-Object @{Name = 'Domain'; Expression = { $domain } }, `
+        Name, SamAccountName, EmailAddress, EmployeeID, Description, LastLogonDate, `
+        @{Name = 'CreatedDate'; Expression = { $_.whenCreated } }, `
+        @{Name = 'ModifiedDate'; Expression = { $_.whenChanged } }, `
+        @{Name = 'GroupMemberships'; Expression = { $_.MemberOf -join "; " } }, `
+        @{Name = 'OU'; Expression = { $_.DistinguishedName -replace '^.*?,(?=[A-Z]{2}=)' } }, `
+        @{Name = 'AccountStatus'; Expression = { if ($_.Enabled -eq $True) { 'Enabled' } else { 'Disabled' } } } | `
         Export-CSV $CSVFileName -NoTypeInformation -Encoding UTF8
 
     Write-S3Object -BucketName $BucketName -File $CSVFileName 
